ff275402ea5bd8ef1f0d85887c29dfe78729df7e
Teerapat <email@github.com>
2026-07-24 20:11:59 +0700

    feat(app): mount vote routers

--- app.mjs
+++ app.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import questionsRouter from "./routers/questions.router.mjs";
 import answersRouter from "./routers/answers.router.mjs";
+import { questionVoteRouter, answerVoteRouter } from "./routers/vote.router.mjs";
 
 const app = express();
 const port = 4000;
@@ -13,6 +14,8 @@ app.get("/test", (req, res) => {
 
 app.use("/questions", questionsRouter);
 app.use("/questions", answersRouter);
+app.use("/questions", questionVoteRouter);
+app.use("/answers", answerVoteRouter);
 
 app.use((req, res) => {
   return res.status(404).json({ message: "Route not found." });
